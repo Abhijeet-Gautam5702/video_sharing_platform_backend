82a32c5f737fc6f3adccafc6a7a761bd1883faad
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-20 23:43:48 +0530

    Add middlewares for necessary configurations like body-parsing, url-payloads and cors

---
+++ src/app.js
@@ -0,0 +1,47 @@
+// Imports
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+/* 
+    `cors() Middleware: Ensure that specific URLs/clients are able to interact with backend
+*/
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+/*
+    `express.json()` Middleware: Used to take JSON inputs from request body
+*/
+app.use(
+    express.json({
+        limit: "16kb",
+    })
+);
+
+/*
+    `express.urlencoded()` Middleware: Used to take inputs from URL-encoded payloads
+*/
+app.use(
+    express.urlencoded({
+        extended: true,
+        limit: "16kb",
+    })
+);
+
+/*
+    `express.static()` Middleware: Used to store static assets like PDFs, images, icons etc.
+*/
+app.use(express.static("public")); // "public" is the name of the folder
+
+/*
+    `cookieParser()` Middleware: Used to store secure cookie which only the Server can read
+*/
+app.use(cookieParser());
+
+export default app;
