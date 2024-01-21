977651d38e6caa6486ed54e4fff635bc4add1da5
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-21 14:47:10 +0530

    Write promisified version of asyncHandler

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -19,3 +19,16 @@ const asyncHandler = (callbackFn) => {
 };
 
 export default asyncHandler;
+
+/*
+    PROMISIFIED VERSION OF THE SAME ASYNC-HANDLER
+
+    const asyncHandler = (callback) => {
+        return (req, res, next) => {
+            return Promise.resolve(callback(req, res, next)).catch((err) =>
+                console.log(err)
+            );
+        };
+    };
+
+*/
