f5aa01e539be3c312a62c51d01909fbe7e113863
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-22 13:42:30 +0530

    Moved utils into the src folder

---
+++ src/utils/apiError.js
@@ -0,0 +1,25 @@
+// Custom class to handle API-errors (extends the built-in Error class in NodeJS)
+
+class apiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+export { apiError };
