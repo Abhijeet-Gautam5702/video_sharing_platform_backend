307c06e4e288e44e2f685838d580517583a780b7
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-24 19:01:56 +0530

    minor bugs in asyncHandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,9 @@
 // A wrapper function which executes async operations using try-catch & async-await syntax
 
 /* 
-NOTE: We will be interacting with async operations many times in the codebase. Instead of writing try-catch block and async-await functions again and again, we have created a wrapper function to avoid code redundancy.
+NOTE-1: We will be interacting with async operations many times in the codebase. Instead of writing try-catch block and async-await functions again and again, we have created a wrapper function to avoid code redundancy.
+
+NOTE-2: Always throw errors in try-catch blocks
 */
 
 const asyncHandler = (callbackFn) => {
@@ -9,11 +11,11 @@ const asyncHandler = (callbackFn) => {
         try {
             await callbackFn(req, res, next);
         } catch (err) {
-            res.status(500).json({
+            res.status(err.statusCode).json({
                 success: false,
                 errorMessage: err.message,
             });
-            throw err;
+            throw err; // IMPORTANT: Otherwise the backend will not send error to the client
         }
     };
 };
