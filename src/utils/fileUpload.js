15a75ab6c15b639f40a544a722d4513f1cbe2db1
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-27 14:13:18 +0530

    Bug-Fix: Add dotenv-config to fix cloudinary configurations

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -2,23 +2,24 @@
     FILE-UPLOAD UTILITY
     - Obtain the file from the client and store it locally in the server (using Multer)
     - Upload the locally stored file to third-party service (here, Cloudinary)
+    - Delete/Unlink the locally stored file from server
 
     NOTE: This is a standard 2-step mechanism followed in most production level backend projects. We can also skip this 2-step process and directly upload files to Cloudinary using Multer.
 */
-
 import fs from "node:fs";
 import { v2 as cloudinary } from "cloudinary";
 import { CLOUDINARY_CLOUD_NAME } from "../constants.js";
 
-// cloudinary.config({
-//     cloud_name: CLOUDINARY_CLOUD_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env",
+});
+
 cloudinary.config({
     cloud_name: CLOUDINARY_CLOUD_NAME,
-    api_key: "558651113423662",
-    api_secret: "5BfFPsfwbNtfteXvCbBWsd7mHZc",
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 async function uploadOnCloudinary(localFilePath) {
@@ -32,13 +33,14 @@ async function uploadOnCloudinary(localFilePath) {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto",
         });
-        console.log(`File uploaded successfully | URL: ${response.url}`);
+        // console.log(`File uploaded successfully | URL: ${response.url}`);
         return response;
     } catch (error) {
-        console.log(error)
-        // uncomment this after testing is done
-        // fs.unlinkSync(localFilePath); // unlink/delete the locally-stored file as the cloudinary-upload failed
+        console.log(error);
         return null;
+    } finally {
+        // unlink/delete the locally-stored file
+        fs.unlinkSync(localFilePath);
     }
 }
 
