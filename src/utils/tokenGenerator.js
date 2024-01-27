3b102cce32880b863a7f480187f761fb7018b4c8
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-27 22:22:11 +0530

    Write utility for token generation

---
+++ src/utils/tokenGenerator.js
@@ -0,0 +1,16 @@
+// Utility to generate tokens
+
+import { apiError } from "./apiError.js";
+
+async function generateAccessAndRefreshTokens(user) {
+    try {
+        const refreshToken = await user.generateRefreshToken();
+        const accessToken = await user.generateAccessToken();
+        const tokens = { refreshToken, accessToken };
+        return tokens;
+    } catch (error) {
+        throw new apiError(500, "Tokens could not be generated from our end");
+    }
+}
+
+export { generateAccessAndRefreshTokens };
