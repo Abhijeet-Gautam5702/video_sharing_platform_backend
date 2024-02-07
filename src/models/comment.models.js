f72066ab819c65d173334fd1137c20f4b06ea1ab
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-02-07 08:11:21 +0530

    Add explanatory comment in src/models/subscription.models.js

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -1,3 +1,6 @@
+// Comment model
+// NOTE: Each comment will belong to one video and one owner only (See the explanation for this in `/src/models/subscription.models.js`)
+
 import mongoose from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
