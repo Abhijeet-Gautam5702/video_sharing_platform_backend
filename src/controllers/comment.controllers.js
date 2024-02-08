14f203194a16ff5c778c1632365a8c1cf455f198
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-02-08 07:25:32 +0530

    Write controllers for comment edit and deletion (With Testing)

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -3,6 +3,7 @@ import Comment from "../models/comment.models.js";
 import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import Video from "../models/video.models.js";
 
 // Get comments on the current video
 const getVideoComments = asyncHandler(async (req, res) => {
@@ -25,23 +26,23 @@ const getVideoComments = asyncHandler(async (req, res) => {
         },
         // STAGE-2: Get the comment-owner details
         {
-            $lookup:{
-                from:"users",
-                localField:"owner",
-                foreignField:"_id",
-                as:"ownerDetails",
-                pipeline:[
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+                pipeline: [
                     // STAGE-2.1: Project only specific details of comment-owner
                     {
-                        $project:{
-                            fullname:1,
-                            username:1,
-                            avatar:1,
-                        }
-                    }
-                ]
-            }
-        }
+                        $project: {
+                            fullname: 1,
+                            username: 1,
+                            avatar: 1,
+                        },
+                    },
+                ],
+            },
+        },
     ]);
     // console.log(comments);
 
@@ -82,7 +83,112 @@ const addComment = asyncHandler(async (req, res) => {
     );
 });
 
-// Update or Edit your comment
 // Delete a comment
+const deleteComment = asyncHandler(async (req, res) => {
+    // Authorization: Check whether the user is authorized to hit this secured route
 
-export { getVideoComments, addComment };
+    // Get userId from req.user
+    const userId = req.user?._id;
+    if (!userId) {
+        throw new apiError(
+            500,
+            "Comment Deletion failed | User-ID not recieved"
+        );
+    }
+
+    // Get videoId and commentId from the URL params
+    const videoId = req.params?.videoId;
+    const commentId = req.params?.commentId;
+    if (!videoId) {
+        throw new apiError(422, "Comment Deletion failed | Invalid Video-ID");
+    }
+    if (!commentId) {
+        throw new apiError(422, "Comment Deletion failed | Invalid Comment-ID");
+    }
+
+    // Check if the user is either the creator of the comment or the owner of the video (Then only they are allowed to delete the comment)
+    const isUserOwnerOfVideo = await Video.findOne({
+        _id: commentId,
+        owner: userId,
+    });
+    const isUserCreatorOfComment = await Comment.findOne({
+        _id: commentId,
+        owner: userId,
+    });
+    if (!isUserCreatorOfComment && !isUserOwnerOfVideo) {
+        throw new apiError(
+            400,
+            "Comment Deletion failed | You cannot delete comments of other users"
+        );
+    }
+
+    // Search for the desired "Comment" document and delete it from database
+    const a = await Comment.deleteOne({
+        video: videoId,
+        owner: userId,
+    });
+    console.log(a);
+
+    // Send success response to the user
+    res.status(200).json(
+        new apiResponse(200, {}, "Comment deleted successfully")
+    );
+});
+
+// Update or Edit your comment
+const editComment = asyncHandler(async (req, res) => {
+    // Authorization: Check if the user is authorized to hit this secure route
+
+    // Get userId from req.user
+    const userId = req.user?._id;
+    if (!userId) {
+        throw new apiError(400, "Cannot edit comment | User-ID not recieved");
+    }
+
+    // Get commentId from req.params and content of comment from body
+    const commentId = req.params?.commentId;
+    const content = req.body?.content;
+    if (!commentId) {
+        throw new apiError(422, "Cannot edit comment | Invalid Comment-ID");
+    }
+    if (!content?.trim()) {
+        throw new apiError(
+            422,
+            "Cannot edit comment | Comment content cannot be empty"
+        );
+    }
+
+    // Check if the user is the owner/creator of the comment
+    const isUserCreatorOfComment = await Comment.findOne({
+        _id: commentId,
+        owner: userId,
+    });
+    if(!isUserCreatorOfComment){
+        throw new apiError(
+            400,
+            "Cannot edit comment | You cannot edit comments of other users"
+        )
+    }
+
+    // Find and update the desired comment
+    const updatedComment = await Comment.findByIdAndUpdate(
+        commentId,
+        {
+            content,
+        },
+        { new: true }
+    );
+    if (!updatedComment) {
+        throw new apiError(
+            500,
+            "Cannot edit comment | Something went wrong from our end"
+        );
+    }
+
+    // Send success response to the user
+    res.status(200).json(
+        new apiResponse(200, updatedComment, "Comment edited successfully")
+    );
+});
+
+export { getVideoComments, addComment, deleteComment, editComment };
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,12 +1,25 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-import { addComment, getVideoComments } from "../controllers/comment.controllers.js";
+import {
+    addComment,
+    deleteComment,
+    editComment,
+    getVideoComments,
+} from "../controllers/comment.controllers.js";
 
 const commentRouter = Router();
 
 // SECURED ROUTES
 commentRouter.route("/comments/:videoId").get(verifyJWT, getVideoComments);
 
-commentRouter.route("/add-comment/:videoId").post(verifyJWT, addComment)
+commentRouter.route("/add-comment/:videoId").post(verifyJWT, addComment);
+
+commentRouter
+    .route("/delete-comment/:videoId/:commentId")
+    .delete(verifyJWT, deleteComment);
+
+commentRouter
+    .route("/edit-comment/:commentId")
+    .put(verifyJWT, editComment);
 
 export { commentRouter };
