aba48c6480c79a49122b5b5c74baf55f510820c6
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-02-17 23:28:48 +0530

    Change Like Model: remove content field

--- src/models/like.models.js
+++ src/models/like.models.js
@@ -2,11 +2,6 @@ import mongoose from "mongoose";
 
 const likeSchema = new mongoose.Schema(
     {
-        content: {
-            type: String,
-            required: true,
-            trim: true,
-        },
         likedBy: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
